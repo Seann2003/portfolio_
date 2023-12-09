9a244b0a54b1b64ad32c0828c089a9b326009365
Seann2003 <seanhoekaizher@gmail.com>
2023-12-09 22:38:30 +0800

    Added Header

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { Inter } from "next/font/google";
+
+
+const inter = Inter({ weight: ['600'], subsets: ['latin'] })
 
 const Layout = ({ children }) => {
     return(
-        <div className="min-h-screen mx-auto ">
+        <div className={`min-h-screen mx-auto ${inter.className}`}>
             <Navbar />
             <main className=" pl-6 pr-6  md:pl-12 md:pr-12 lg:pr-12 lg:pl-12">{children}</main>
         </div>
